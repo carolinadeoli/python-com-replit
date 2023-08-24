c057e1dc86e1c0247591c1552343e60ac191dcc9
carolinadeoli <25742649-carolinadeoli@users.noreply.replit.com>
2023-08-24 13:17:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1/ n2
+print(f'{n1} + {n2} = {soma} ')
+print(f'{n1} - {n2} = {subtracao}')
+print(f'{n1} * {n2}= {multiplicacao}')
+print(f'{n1} / {n2} = {divisao}' )
