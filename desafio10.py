3194864ed1e4d2306bec2faf44edaf2fb5d33ab2
carolinadeoli <121107004+carolinadeoli@users.noreply.github.com>
2023-09-19 14:26:50 +0000

    desafios

---
+++ desafio10.py
@@ -0,0 +1,4 @@
+frutas = ['maçã', 'morango', 'uva']
+
+for f in frutas:
+  print(f'Eu gosto de {f}')
---
+++ nestedloops.py
@@ -0,0 +1,6 @@
+vegetais = ['alface', 'couve','brócolis']
+frutas = ['banana','morango','maçã']
+
+for f in frutas:
+  for v in vegetais:
+    print(f,v)
